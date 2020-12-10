ade6c186f1841e67cd144368d18122025befd41c
Nicolau NP <mr1Normaliii@gmail.com>
2020-12-10 09:21:54 +0000

    update

--- app/Http/Controllers/ControladorStatic.php
+++ app/Http/Controllers/ControladorStatic.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Pagamento;
 use App\PagamentoPai;
 use App\TipoPagamento;
 use Illuminate\Http\Request;
@@ -19,14 +20,19 @@ class ControladorStatic extends Controller
     }
 
     public static function getValoresBalanco($epoca, $id_tipo_pagamento, $ano_lectivo){
-        
+        $retorno = 0;
         $data = [
             'epoca'=>$epoca,
             'id_tipo_pagamento'=>$id_tipo_pagamento,
             'ano_lectivo'=>$ano_lectivo,
         ];
 
+        if($id_tipo_pagamento == 3){
+            $pagamento = PagamentoPai::where('id_tipo_pagamento', $id_tipo_pagamento)->get();
+        }else{
+            $pagamento = Pagamento::where('id_tipo_pagamento', $id_tipo_pagamento)->get();
+        }
         
-        return 233;
+        return $retorno;
     }
 }

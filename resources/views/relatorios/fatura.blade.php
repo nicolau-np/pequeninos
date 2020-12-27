55f20effc8a6648203395c81cc6a83dfdbcc74de
Nicolau NP <mr1Normaliii@gmail.com>
2020-12-27 12:55:57 +0000

    terminando fatura

--- resources/views/relatorios/fatura.blade.php
+++ resources/views/relatorios/fatura.blade.php
@@ -83,9 +83,9 @@
    <div class="body">
        @if ($getId_tipo_pagamento == 3)
              <div class="tableComparticipacao">
-                <table border=1 cellspacing=0 cellpadding=2 bordercolor="#000">
+                <table border=1 cellspacing=0 cellpadding=2 bordercolor="#000" style="width: 60%">
                     <thead>
-                        <tr>
+                        <tr class="tr_especial">
                         <th>Encarregado</th>
                         <th>Educandos</th>
                         </tr>
@@ -103,12 +103,12 @@
                     </tbody>
                 </table>
 
-                <hr/>
-                <table border=1 cellspacing=0 cellpadding=2 bordercolor="#000">
+                <br/>
+                <table border=1 cellspacing=0 cellpadding=2 bordercolor="#000" style="width: 60%;">
                     <thead>
                         <tr class="tr_especial">
                             <th>Epoca</th>
-                            <th>Valor</th>
+                            <th>Valor(Akz)</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -122,7 +122,7 @@
                         ?>
                             <tr>
                             <td>{{$pagamento->epoca}}</td>
-                            <td>{{number_format($pagamento->preco,2,',','.')}} Akz</td>
+                            <td>{{number_format($pagamento->preco,2,',','.')}}</td>
                             </tr>
                         <?php }?>
                        
@@ -199,5 +199,157 @@
         </p>
      </div>
     
-</body>
+
+
+
+
+     <div class="header">
+        <div class="desc_empresa">
+        <p>
+               <span style="font-weight: bold;">ESCOLA PRIMÁRIA E Iº CÍCLO LAR DOS PEQUENINOS <BR/>
+               DAS IRMÃS DO SANTÍSSIMO SALVADOR-HUAMBO</span><BR/>
+               NIF: 5121019874<BR/>
+        </p>
+        </div>
+        <div class="desc_cliente">
+            <p>
+              Exmo Sr(a).  <BR/>
+              <span style="font-weight: bold;">
+                {{$getHistorico->estudante->encarregado->pessoa->nome}}<br/>
+                HUAMBO-ANGOLA
+              </span>
+            </p>
+            
+        </div>
+        <br/><br/><br/><br/>
+    </div>
+
+    <div class="header-body">
+        <br/><br/><br/><br/>
+        <table border=1 cellspacing=0 cellpadding=2 bordercolor="#000" class="tabela1">
+            <tr>
+                <td colspan="2">PAGAMENTO DE {{strtoupper($getTipoPagamento->tipo)}}</td>
+            </tr>
+            <tr>
+                <td>Fact. Nº {{date('dmY', strtotime($getFatura->data_fatura))}}{{$getFatura->id}}</td>
+            <td>Data: {{date('d-m-Y', strtotime($getFatura->data_fatura))}} {{date('H:i:s', strtotime($getFatura->created_at))}}</td>
+            </tr>
+        </table>
+    </div>
+
+   <div class="body">
+       @if ($getId_tipo_pagamento == 3)
+             <div class="tableComparticipacao">
+                <table border=1 cellspacing=0 cellpadding=2 bordercolor="#000" style="width: 60%">
+                    <thead>
+                        <tr class="tr_especial">
+                        <th>Encarregado</th>
+                        <th>Educandos</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>{{$getHistorico->estudante->encarregado->pessoa->nome}}</td>
+                        <td>
+                            @foreach ($getEducandos as $educandos)
+                                {{$educandos->pessoa->nome}}<br/>
+                            @endforeach
+                        </td>
+                       </tr>
+                     
+                    </tbody>
+                </table>
+
+                <br/>
+                <table border=1 cellspacing=0 cellpadding=2 bordercolor="#000" style="width: 60%;">
+                    <thead>
+                        <tr class="tr_especial">
+                            <th>Epoca</th>
+                            <th>Valor(Akz)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $total = 0;
+                        $created_at = null;
+                        
+                        foreach ($getPagamento as $pagamento){
+                        $created_at = $pagamento->created_at;
+                        $total = $total + $pagamento->preco;
+                        ?>
+                            <tr>
+                            <td>{{$pagamento->epoca}}</td>
+                            <td>{{number_format($pagamento->preco,2,',','.')}}</td>
+                            </tr>
+                        <?php }?>
+                       
+                    </tbody>
+                </table>
+                <p style="font-size:14px;">
+                    Total Geral: {{number_format($total,2,',','.')}} Akz
+                </p>
+            </div>
+
+        @else
+        <div class="tablePagamento">
+            <table border=1 cellspacing=0 cellpadding=2 bordercolor="#000" style="width: 100%">
+                <tr class="tr_especial">
+                    <td>Nº Processo</td>
+                    <td>Nome completo</td>
+                    <td>Classe</td>
+                    <td>Turma</td>
+                    <td>Turno</td>
+                </tr>
+                <tr>
+                <td>{{$getHistorico->id_estudante}}</td>
+                <td>{{$getHistorico->estudante->pessoa->nome}}</td>
+                <td>{{$getHistorico->turma->classe->classe}}</td>
+                <td>{{$getHistorico->turma->turma}}</td>
+                <td>{{$getHistorico->turma->turno->turno}}</td>
+                </tr>
+            </table>
+            <br/>
+            <table border=1 cellspacing=0 cellpadding=2 bordercolor="#000" style="width: 60%;">
+                <thead>
+                    <tr class="tr_especial">
+                        <th>Epoca</th>
+                        <th>Valor (Akz)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $total = 0;
+                    $created_at = null;
+                    
+                    foreach ($getPagamento as $pagamento){
+                    $created_at = $pagamento->created_at;
+                    $total = $total + $pagamento->preco;
+                    ?>
+                        <tr>
+                        <td>{{$pagamento->epoca}}</td>
+                        <td>{{number_format($pagamento->preco,2,',','.')}}</td>
+                        </tr>
+                    <?php }?>
+                   
+                </tbody>
+            </table>
+            <p style="font-size:14px;">
+                Total Geral: {{number_format($total,2,',','.')}} Akz
+            </p>
+        </div>
+       @endif
+    </div>
+    <div class="rodape">
+        <div class="encarregado">
+            Encarregado<br/>
+        ________________________
+        </div>
+        <div class="secretaria">
+            Secretaria<br/>
+            ______________________
+        </div>
+    </div>
+
+
+    </body>
 </html>

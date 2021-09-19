545debc0b3154915462f653c2a85e80b7a2277f1
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-19 11:34:31 +0100

    mudando os anos da seeder

--- database/seeds/AnoLectivoSeeder.php
+++ database/seeds/AnoLectivoSeeder.php
@@ -6,15 +6,14 @@ use Illuminate\Support\Facades\DB;
 class AnoLectivoSeeder extends Seeder
 {
    static $ano_lectivos = [
-     ['ano_lectivo'=>"2019"],
-     ['ano_lectivo'=>"2020"],
-     ['ano_lectivo'=>"2021"],
+     ['ano_lectivo'=>"2020-2021"],
+     ['ano_lectivo'=>"2021-2022"],
    ];
     public function run()
     {
         foreach(Self::$ano_lectivos as $ano_lectivo){
             DB::table('ano_lectivos')->insert(
-              $ano_lectivo  
+              $ano_lectivo
             );
         }
     }

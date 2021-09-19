d4d8f211dcb0eaf70f81c045b8526b4eaee9490b
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-19 11:58:58 +0100

    trabalhando na migration da observacao unica

--- database/migrations/2021_09_19_114554_create_observacao_unicas_table.php
+++ database/migrations/2021_09_19_114554_create_observacao_unicas_table.php
@@ -14,8 +14,19 @@ class CreateObservacaoUnicasTable extends Migration
     public function up()
     {
         Schema::create('observacao_unicas', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->bigInteger('id_curso')->unsigned()->index();
+            $table->bigInteger('id_classe')->unsigned()->index();
+            $table->bigInteger('id_disciplina')->unsigned()->index();
+            $table->bigInteger('quantidade_negativas');
+            $table->string('estado');
             $table->timestamps();
+        });
+
+        Schema::table('observacao_unicas', function (Blueprint $table){
+            $table->foreign('id_curso')->references('id')->on('cursos')->onUpdate('cascade');
+            $table->foreign('id_classe')->references('id')->on('classes')->onUpdate('cascade');
+            $table->foreign('id_disciplina')->references('id')->on('disciplinas')->onUpdate('cascade');
         });
     }
 

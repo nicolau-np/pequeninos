460111f594fd9fe3ec57f3ac79b60cb8e680a81a
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-19 12:01:22 +0100

    trabalhando no modelo da observacao unica

--- app/ObservacaoUnica.php
+++ app/ObservacaoUnica.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ObservacaoUnica extends Model
 {
-    //
+    protected $table = "observacao_unicas";
+
+    protected $fillable = [
+        'id_curso',
+        'id_classe',
+        'id_disciplina',
+        'quantidade_negativas',
+        'estado',
+    ];
 }

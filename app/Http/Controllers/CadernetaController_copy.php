44ef6f0d7c2f10a0dda66469c09382d46e9043cc
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-20 11:38:12 +0100

    updating caderneta

--- app/Http/Controllers/CadernetaController_copy.php
+++ app/Http/Controllers/CadernetaController_copy.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Funcionario;
+use App\Horario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class CadernetaController_copy extends Controller
 {
     public function index(){
-        
+        $id_pessoa = Auth::user()->pessoa->id;
+        $funcionario = Funcionario::where('id_pessoa', $id_pessoa)->first();
+        $data['where'] = [
+            'id_funcionario' => $funcionario->id,
+            'estado' => "visivel",
+        ];
+        $horarios = Horario::where($data['where'])->orderBy('ano_lectivo', 'desc')->paginate(8);
+        Session::put('id_funcionario', $funcionario->id);
+        $data = [
+            'title' => "Caderneta",
+            'type' => "caderneta",
+            'menu' => "Caderneta",
+            'submenu' => "Listar",
+            'getHorario' => $horarios,
+        ];
+        return view('caderneta.list', $data);
     }
 }

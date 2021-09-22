2eca5d8be7640130e718d7f09a890f954c50488e
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-22 08:15:16 +0100

    trabalhando na lista de turmas

--- app/Http/Controllers/TurmasController.php
+++ app/Http/Controllers/TurmasController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Ensino;
 use Illuminate\Http\Request;
 
 class TurmasController extends Controller
 {
     public function index(){
-        echo "tur,as";
+        $ensinos = Ensino::all();
+        $data = [
+            'title' => "Turmas",
+            'type' => "turmas",
+            'menu' => "Turmas",
+            'submenu' => "Listar",
+            'getEnsinos' => $ensinos,
+        ];
+        return view('turmas.list', $data);
     }
 }

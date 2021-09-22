15b403deae1336ffd62de1626345f0276fad52fc
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-22 15:39:44 +0100

    update

--- resources/views/relatorios/lista_encarregados.blade.php
+++ resources/views/relatorios/lista_encarregados.blade.php
@@ -1,4 +1,4 @@
-@php 
+@php
 use App\Http\Controllers\ControladorStatic;
 $trimestres = [
     '1 Trimestre', '2 Trimestre', '3 Trimestre',
@@ -51,43 +51,43 @@ $trimestre_totais = [
                     <th>Telefone</th>
                     <th>Educandos</th>
                     @foreach ($trimestres as $epoca)
-                       <th>{{$epoca}}</th> 
+                       <th>{{$epoca}}</th>
                     @endforeach
                 </tr>
             </thead>
             <tbody>
                 @foreach ($getEncarregados as $encarregados)
                 <tr>
-                    <td>{{$loop->iteration}}</td> 
+                    <td>{{$loop->iteration}}</td>
                     <td>{{$encarregados->pessoa->nome}}</td>
                     <td>{{$encarregados->pessoa->telefone}}</td>
                     <td>
-                        <?php 
+                        <?php
                         $estudantes = ControladorStatic::getEducandos($encarregados->id);
                         foreach ($estudantes as $estudante) {
                         ?>
                         {{$estudante->pessoa->nome}} <br/>
-                        <?php } 
+                        <?php }
                         ?>
                     </td>
 
                     @foreach ($trimestres as $epoca)
-              
+
                     <td>
-                     <?php 
-                       $valores = ControladorStatic::getValoresComparticipacao($epoca, $encarregados->id, $getAno->ano_lectivo);    
+                     <?php
+                       $valores = ControladorStatic::getValoresComparticipacao($epoca, $encarregados->id, $getAno->ano_lectivo);
                      ?>
                      @if($valores==null)
                      ---
                      @else
-                        {{number_format($valores->preco,2,',','.')}} Akz   
+                        {{number_format($valores->preco,2,',','.')}} Akz
                     @endif
                     <?php ?>
-                    </td> 
+                    </td>
                     @endforeach
                 </tr>
                 @endforeach
-               
+
             </tbody>
         </table>
     </div>

13cf9cdbe476c54170d08159b5e5d8d75164e0fa
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-22 08:07:10 +0100

    criando pagina de erro 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Erro 404</title>
+</head>
+<body>
+<div style="text-align: center; margin-top:10%;">
+<h1>Página Não encontrada</h1>
+<br/>
+<p>Voltar para a <a href="/">Página Principal</a></p>
+</div>
+</body>
+</html>

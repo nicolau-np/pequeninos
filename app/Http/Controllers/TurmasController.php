a078aef5b41d4694c138114330c8621f3cd23055
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-22 09:25:23 +0100

    update

--- app/Http/Controllers/TurmasController.php
+++ app/Http/Controllers/TurmasController.php
@@ -9,8 +9,8 @@ use Illuminate\Http\Request;
 class TurmasController extends Controller
 {
     public function index($ano_lectivo){
-        $ano_lectivo = AnoLectivo::where(['ano_lectivo'=>$ano_lectivo])->first();
-        if(!$ano_lectivo){
+        $ano_lectivoV = AnoLectivo::where(['ano_lectivo'=>$ano_lectivo])->first();
+        if(!$ano_lectivoV){
             return back()->with(['error' => "Não encontrou"]);
         }
         $ensinos = Ensino::orderBy('id', 'asc')->get();
@@ -21,7 +21,7 @@ class TurmasController extends Controller
             'menu' => "Turmas",
             'submenu' => "Listar",
             'getEnsinos' => $ensinos,
-            'getAno'=>"2021-2022",
+            'getAno'=>$ano_lectivo,
             'getAnos'=>$ano_lectivos,
         ];
         return view('turmas.list', $data);

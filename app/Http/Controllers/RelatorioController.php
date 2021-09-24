71b738dd6c72645d682db7ffe93321d990f2b43d
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-24 08:05:15 +0100

    criado modelo basico

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -209,24 +209,28 @@ class RelatorioController extends Controller
             'getHorario' => $horario,
             'getHistorico' => $historico,
         ];
-        $pdf = PDF::loadView('relatorios.lista_nominal', $data)->setPaper('A4', 'normal');
+
 
         if ($id_ensino == 1) {//iniciacao ate 6
             //se for classificacao quantitativa
             if(($classe=="2ª classe") || ($classe=="4ª classe") || ($classe=="6ª classe")){
-                return view('minipauta.pdf.ensino_primario_2_4_6_copy', $data);
+                $pdf = PDF::loadView('minipauta.pdf.ensino_primario_2_4_6_copy', $data)->setPaper('A4', 'normal');
+
             }//se for classificacao quantitativa
             elseif(($classe=="Iniciação") || ($classe=="1ª classe") || ($classe=="3ª classe") || ($classe=="5ª classe")){
-                return view('minipauta.pdf.ensino_primario_Ini_1_3_5_copy', $data);
+                $pdf = PDF::loadView('minipauta.pdf.ensino_primario_Ini_1_3_5_copy', $data)->setPaper('A4', 'normal');
+
             }
         } elseif ($id_ensino == 2) {//7 classe ate 9 ensino geral
             if($classe == "9ª classe"){
-                return view('minipauta.pdf.ensino_1ciclo_9_copy', $data);
+                $pdf = PDF::loadView('minipauta.pdf.ensino_1ciclo_9_copy', $data)->setPaper('A4', 'normal');
+
             }else{
-                return view('minipauta.pdf.ensino_1ciclo_7_8_copy', $data);
+                $pdf = PDF::loadView('minipauta.pdf.ensino_1ciclo_7_8_copy', $data)->setPaper('A4', 'normal');
+
             }
         }
 
-        return $pdf->stream('Lista Nominal ' . $turma->turma . '' . $ano_lectivo . '.pdf');
+        return $pdf->stream('Mini Pauta '.$ano_lectivo.'[' . $turma->turma . ' ' . $turma->turno->turno .'-'.$turma->curso->curso.'-'.$horario->disciplina->disciplina. '].pdf');
     }
 }
--- resources/views/minipauta/pdf/ensino_1ciclo_9_copy.blade.php
+++ resources/views/minipauta/pdf/ensino_1ciclo_9_copy.blade.php
@@ -1,1 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Mini Pauta {{$getHorario->ano_lectivo}} [{{$getHorario->turma->turma}}-{{$getHorario->turma->turno->turno}}-{{$getHorario->turma->curso->curso}}-{{$getHorario->disciplina->disciplina}} ]</title>
+</head>
+<body>
 hello
+</body>
+</html>

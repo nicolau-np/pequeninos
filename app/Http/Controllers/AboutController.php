e39e00b7f5f35b6bbb5bdbf4c71af5f7e90922e0
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-24 01:40:24 +0100

    trabalhando nos metodos do controller About

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -7,10 +7,24 @@ use Illuminate\Http\Request;
 class AboutController extends Controller
 {
     public function sistema(){
+        $data = [
+            'title' => "Sistema",
+            'type' => "sistema",
+            'menu' => "Sistema",
+            'submenu' => "Sobre",
 
+       ];
+        return view('about.sistema', $data);
     }
 
     public function instituicao(){
-        
+        $data = [
+            'title' => "Sistema",
+            'type' => "sistema",
+            'menu' => "Instituição",
+            'submenu' => "Sobre",
+
+       ];
+        return view('about.instituicao', $data);
     }
 }

0ba6108cebf10af46212b72487881426308d25f8
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-01 19:52:41 +0100

    adicionando novo cabecalho

--- resources/views/include/header_docs.blade.php
+++ resources/views/include/header_docs.blade.php
@@ -1,8 +1,9 @@
 <div>
     <p style="text-align: center;">
-        COMPLEXO ESCOLAR PRIVADO<br/>
-        ENSINO PARTICULAR<br/>
-        SÍTIO DOS PEQUENINOS-MOÇÂMEDES<br/>
+        REPÚBLICA DE ANGOLA<br/>
+        MINISTÉRIO DA EDUCAÇÃO<br/>
+        COMPLEXO ESCOLAR Nº 89M "EDUARDO DOMINGOS SUKUETE"<br/>
+        = MOÇÂMEDES - NAMIBE =
     </p>
 </div>
 
--- resources/views/include/header_docs_left.blade.php
+++ resources/views/include/header_docs_left.blade.php
@@ -1,8 +1,7 @@
 <p>
     <span style="font-weight: bold;">
-    COMPLEXO ESCOLAR PRIVADO <br/>
-    ENSINO PARTICULAR<br/>
-    SÍTIO DOS PEQUENINOS-MOÇÂMEDES
+        COMPLEXO ESCOLAR Nº 89M "EDUARDO DOMINGOS SUKUETE"<br/>
+        = MOÇÂMEDES - NAMIBE =
     </span><br/>
     NIF: 5121019874<br/>
 </p>

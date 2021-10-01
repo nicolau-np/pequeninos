b0d7969ad771f3b63b98c1e31df2eb1f65a2a036
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-01 06:22:33 +0100

    update mini pauta

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -209,11 +209,13 @@ class RelatorioController extends Controller
 
         if ($id_ensino == 1) { //iniciacao ate 6
             //se for classificacao quantitativa
-            if (($classe == "2ª classe") || ($classe == "4ª classe") || ($classe == "6ª classe")) {
-                $pdf = PDF::loadView('minipauta.pdf.ensino_primario_2_4_6_copy', $data['view'])->setPaper('A4', 'normal');
+            if (($classe == "2ª classe") || ($classe == "4ª classe")) {
+                $pdf = PDF::loadView('minipauta.pdf.ensino_primario_2_4_copy', $data['view'])->setPaper('A4', 'normal');
             } //se for classificacao quantitativa
             elseif (($classe == "Iniciação") || ($classe == "1ª classe") || ($classe == "3ª classe") || ($classe == "5ª classe")) {
                 $pdf = PDF::loadView('minipauta.pdf.ensino_primario_Ini_1_3_5_copy', $data['view'])->setPaper('A4', 'normal');
+            }elseif(($classe == "6ª classe")){
+                $pdf = PDF::loadView('minipauta.pdf.ensino_primario_6_copy', $data['view'])->setPaper('A4', 'normal');
             }
         } elseif ($id_ensino == 2) { //7 classe ate 9 ensino geral
             if ($classe == "9ª classe") {

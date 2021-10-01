778e954ce641ac2303d03d3e93d8ca25d38bb2c1
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-01 19:55:19 +0100

    adicionando insignia

--- resources/views/include/header_docs.blade.php
+++ resources/views/include/header_docs.blade.php
@@ -1,5 +1,8 @@
 <div>
+
     <p style="text-align: center;">
+        <img src="https://www.embassyangolatr.org/wp-content/uploads/2019/02/png13.png"
+        alt="" srcset="" style="height:50px; width:50px;"/><br/><br/>
         REPÚBLICA DE ANGOLA<br/>
         MINISTÉRIO DA EDUCAÇÃO<br/>
         COMPLEXO ESCOLAR Nº 89M "EDUARDO DOMINGOS SUKUETE"<br/>

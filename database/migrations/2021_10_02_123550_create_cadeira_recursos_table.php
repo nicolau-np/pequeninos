52c366d0e8d6109b8c8a1de7a584d4978cd53dd2
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-02 12:42:00 +0100

    trabalhando na migration cadeiras recurso

--- database/migrations/2021_10_02_123550_create_cadeira_recursos_table.php
+++ database/migrations/2021_10_02_123550_create_cadeira_recursos_table.php
@@ -14,8 +14,18 @@ class CreateCadeiraRecursosTable extends Migration
     public function up()
     {
         Schema::create('cadeira_recursos', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->bigInteger('id_curso')->unsigned()->index();
+            $table->bigInteger('id_classe')->unsigned()->index();
+            $table->bigInteger('id_disciplina')->unsigned()->index();
+            $table->string('estado');
             $table->timestamps();
+        });
+
+        Schema::table('cadeira_recursos', function (Blueprint $table) {
+            $table->foreign('id_curso')->references('id')->on('cursos')->onUpdate('cascade');
+            $table->foreign('id_classe')->references('id')->on('classes')->onUpdate('cascade');
+            $table->foreign('id_disciplina')->references('id')->on('disciplinas')->onUpdate('cascade');
         });
     }
 

f10a32e807d2deb0b3c8337ed81ee7c1b0f4e9f3
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-02 16:35:55 +0100

    trabalhando no modelo cadeira exame

--- app/CadeiraExame.php
+++ app/CadeiraExame.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class CadeiraExame extends Model
 {
-    //
+    protected $table = "cadeira_exames";
+
+    protected $fillable = [
+        'id_curso',
+        'id_classe',
+        'id_disciplina',
+        'estado',
+    ];
+
+    public function curso(){
+        return $this->belongsTo(Curso::class, 'id_curso', 'id');
+    }
+
+    public function classe(){
+        return $this->belongsTo(Classe::class, 'id_classe', 'id');
+    }
+
+    public function disciplina(){
+        return $this->belongsTo(Disciplina::class, 'id_disciplina', 'id');
+    }
 }

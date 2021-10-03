2975e337c69c7e57322ee49f5330e8cf7f6bc6ef
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-03 08:40:47 +0100

    update

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -429,18 +429,18 @@ class RelatorioController extends Controller
             //se for classificacao quantitativa
             if (($classe == "2ª classe") || ($classe == "4ª classe")) {
 
-               $pdf = PDF::loadView('relatorios.ensinos.boletins.ensino_primario_2_4_copy', $data['view'])->setPaper('A4', 'landscape');
+               $pdf = PDF::loadView('relatorios.ensinos.boletins.ensino_primario_2_4_copy', $data['view'])->setPaper('A4', 'normal');
             } //se for classificacao quantitativa
             elseif (($classe == "Iniciação") || ($classe == "1ª classe") || ($classe == "3ª classe") || ($classe == "5ª classe")) {
-                $pdf = PDF::loadView('relatorios.ensinos.boletins.ensino_primario_Ini_1_3_5_copy', $data['view'])->setPaper('A4', 'landscape');
+                $pdf = PDF::loadView('relatorios.ensinos.boletins.ensino_primario_Ini_1_3_5_copy', $data['view'])->setPaper('A4', 'normal');
             }elseif(($classe == "6ª classe")){
-                $pdf = PDF::loadView('relatorios.ensinos.boletins.ensino_primario_6_copy', $data['view'])->setPaper('A4', 'landscape');
+                $pdf = PDF::loadView('relatorios.ensinos.boletins.ensino_primario_6_copy', $data['view'])->setPaper('A4', 'normal');
             }
         } elseif ($id_ensino == 2) { //7 classe ate 9 ensino geral
             if ($classe == "9ª classe") {
-                $pdf = PDF::loadView('relatorios.ensinos.boletins.ensino_1ciclo_9_copy', $data['view'])->setPaper('A4', 'landscape');
+                $pdf = PDF::loadView('relatorios.ensinos.boletins.ensino_1ciclo_9_copy', $data['view'])->setPaper('A4', 'normal');
             } else {
-                $pdf = PDF::loadView('relatorios.ensinos.boletins.ensino_1ciclo_7_8_copy', $data['view'])->setPaper('A4', 'landscape');
+                $pdf = PDF::loadView('relatorios.ensinos.boletins.ensino_1ciclo_7_8_copy', $data['view'])->setPaper('A4', 'normal');
             }
         }
         return $pdf->stream('BOLETIM DE NOTAS ' .$request->epoca.'º TRIMESTRE - ' . $ano_lectivo . '[ ' . strtoupper($turma->turma) . ' ' . strtoupper($turma->turno->turno) . '-' . strtoupper($turma->curso->curso) . ' ].pdf');
--- resources/views/relatorios/ensinos/boletins/ensino_1ciclo_9_copy.blade.php
+++ resources/views/relatorios/ensinos/boletins/ensino_1ciclo_9_copy.blade.php
@@ -105,56 +105,7 @@ use App\Http\Controllers\ControladorNotas;
                  <span class="title">Nome completo:</span> {{$historico->estudante->pessoa->nome}}
              </div>
              <div class="table-responsive">
-                 <table class="tabela" border="1" cellspacing=0 cellpadding=2 bordercolor="#000" style="width: 100%;">
-                    <thead>
-
-                        <tr>
-                            @foreach (Session::get('disciplinas') as $disciplina)
-                            <?php
-                            $getDisciplina = ControladorStatic::getDisciplinaID($disciplina['id_disciplina']);
-                            ?>
-                           <th colspan="4">{{$getDisciplina->disciplina}}</th>
-                           @endforeach
-                        </tr>
-
-                        <tr>
-                          @foreach (Session::get('disciplinas') as $disciplina)
-                            <th>MAC{{$getEpoca}}</th>
-                            <th>NPP{{$getEpoca}}</th>
-                            <th>PT{{$getEpoca}}</th>
-                            <th>MT{{$getEpoca}}</th>
-                          @endforeach
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            @foreach (Session::get('disciplinas') as $disciplina)
-                            <?php
-                            $trimestrel = ControladorNotas::getValoresMiniPautaTrimestralPDF($disciplina['id_disciplina'], $historico->id_estudante, $getEpoca, $getDirector->ano_lectivo);
-                            if($trimestrel->count()==0){
-                        ?>
-                        <td>---</td>
-                        <td>---</td>
-                        <td>---</td>
-                        <td>---</td>
-                            <?php }
-                            else{
-                                foreach($trimestrel as $valor1){
-                                    $v1_estilo = ControladorNotas::nota_20($valor1->mac);
-                                    $v2_estilo = ControladorNotas::nota_20($valor1->npp);
-                                    $v3_estilo = ControladorNotas::nota_20($valor1->pt);
-                                    $v4_estilo = ControladorNotas::nota_20($valor1->mt);
-                                ?>
-
-                        <td class="{{$v1_estilo}}">@if($valor1->mac==null) --- @else {{$valor1->mac}} @endif</td>
-                        <td class="{{$v2_estilo}}">@if($valor1->npp==null) --- @else {{$valor1->npp}} @endif</td>
-                        <td class="{{$v3_estilo}}">@if($valor1->pt==null) --- @else {{$valor1->pt}} @endif</td>
-                        <td class="{{$v4_estilo}}">@if($valor1->mt==null) --- @else {{$valor1->mt}} @endif</td>
-                                <?php }}?>
-                                    @endforeach
-                            </tr>
-                    </tbody>
-                </table>
+                 
              </div>
             </div>
              @endforeach

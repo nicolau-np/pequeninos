c350d55ad7da7c6b26642771bf155b3a9e757b22
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-03 08:27:44 +0100

    update

--- resources/views/pauta/pdf/ensino_primario_6_copy.blade.php
+++ resources/views/pauta/pdf/ensino_primario_6_copy.blade.php
@@ -1,12 +1,9 @@
 @php
 use App\Http\Controllers\ControladorNotas;
 use App\Http\Controllers\ControladorStatic;
-$observacao_geral = ControladorNotas::observacao_geral($getDirector->turma->classe->id,$getDirector->turma->curso->id);
-if(!$observacao_geral){
-    $observacao_geralDB=22;
-}else{
-    $observacao_geralDB= $observacao_geral->quantidade_negativas;
-}
+$numero_colspan = 2;
+$getCadeiraExame = false;
+$getCadeiraRecurso = false;
 @endphp
 
 <!DOCTYPE html>
@@ -110,125 +107,103 @@ if(!$observacao_geral){
          </div>
          <br/>
          <div class="corpo">
-            <div class="table-responsive">
-                <table class="tabela" border="1" cellspacing=0 cellpadding=2 bordercolor="#000" style="width: 100%;">
-                    <thead>
+            <table class="tabela" border="1" cellspacing=0 cellpadding=2 bordercolor="#000" style="width: 100%;">
+                <thead>
 
-                        <tr>
-                            <th rowspan="2">Nº</th>
-                            <th rowspan="2">NOME COMPLETO</th>
-                            <th rowspan="2">G</th>
-                            <?php
-                            foreach(Session::get('disciplinas') as $disciplina){
-                              $getDisciplina = ControladorStatic::getDisciplinaID($disciplina['id_disciplina'])
-                              ?>
-                            <th colspan="3">{{strtoupper($getDisciplina->disciplina)}}</th>
-                            <?php } ?>
-                            <th rowspan="2">OBSERVAÇÃO</th>
-                        </tr>
-                        <tr>
-                          @foreach (Session::get('disciplinas') as $disciplina)
-                          <th>MFD</th>
-                          <th>NPE</th>
-                          <th>MF</th>
-                          @endforeach
-                       </tr>
-                    </thead>
-                    <tbody>
+                    <tr>
+                        <th rowspan="2">Nº</th>
+                        <th rowspan="2">NOME COMPLETO</th>
+                        <th rowspan="2">G</th>
                         <?php
-                          $defice_disciplinas = [];
-                          $count_obs=0;
-                          $observacao_final = false;
 
-                          $numero_cadeiras = 0;
-                          $numero_lancados = 0;
-                        ?>
-                      @foreach ($getHistorico as $historico)
-                          <?php
-                                  $numero_cadeiras = 0;
-                                  $numero_lancados = 0;
-                                  $observacao_final = false;
-                                  $count_obs = 0;
-                                  $observacao_especifica=false;
+                        foreach(Session::get('disciplinas') as $disciplina){
+                          $numero_colspan = 2;
+                          $getDisciplina = ControladorStatic::getDisciplinaID($disciplina['id_disciplina']);
+                          $getCadeiraExame = ControladorStatic::getExameStatus($getDirector->turma->id_curso, $getDirector->turma->id_classe, $disciplina['id_disciplina']);
+                          $getCadeiraRecurso = ControladorStatic::getRecursoStatus($getDirector->turma->id_curso, $getDirector->turma->id_classe, $disciplina['id_disciplina']);
 
-                                  $defice_disciplinas = [];
+                          if($getCadeiraExame){
+                              $numero_colspan = $numero_colspan + 1;
+                          }
+
+                          if($getCadeiraRecurso){
+                              $numero_colspan = $numero_colspan + 1;
+                          }
                           ?>
-                       <tr class="{{$historico->observacao_final}}">
-                          <td>{{$loop->iteration}}</td>
-                          <td>{{$historico->estudante->pessoa->nome}}</td>
-                          <td>{{$historico->estudante->pessoa->genero}}</td>
+                        <th colspan="{{$numero_colspan}}">{{strtoupper($getDisciplina->disciplina)}}</th>
+                        <?php } ?>
+                        <th rowspan="2">OBSERVAÇÃO</th>
+                    </tr>
+                    <tr>
+                      @foreach (Session::get('disciplinas') as $disciplina)
+                      <?php
+                          $getCadeiraExame = ControladorStatic::getExameStatus($getDirector->turma->id_curso, $getDirector->turma->id_classe, $disciplina['id_disciplina']);
+                          $getCadeiraRecurso = ControladorStatic::getRecursoStatus($getDirector->turma->id_curso, $getDirector->turma->id_classe, $disciplina['id_disciplina']);
+                      ?>
+                      <th>MFD</th>
+                      @if($getCadeiraExame)
+                      <th>NPE</th>
+                      @endif
+                      <th>MF</th>
+                      @if($getCadeiraRecurso)
+                      <th>REC</th>
+                      @endif
+                      @endforeach
+                   </tr>
+                </thead>
+                <tbody>
 
-                          <?php
-                          foreach (Session::get('disciplinas') as $disciplina) {
-                              $numero_cadeiras = $numero_cadeiras + 1;
-                              $final = ControladorNotas::getValoresPautaFinalPDF($historico->id_estudante, $disciplina["id_disciplina"], $getDirector->ano_lectivo);
-                              if($final->count() == 0){
-                              ?>
-                              <td>---</td>
-                              <td>---</td>
-                              <td>---</td>
-                          <?php } else {
-                              foreach ($final as $valorf) {
-                              $v1_estilo = ControladorNotas::nota_10($valorf->mfd);
-                              $v2_estilo = ControladorNotas::nota_10($valorf->npe);
-                              $v3_estilo = ControladorNotas::nota_10($valorf->mf);
-                              ?>
-                              <td class="{{$v1_estilo}}">@if($valorf->mfd == null) --- @else {{$valorf->mfd}} @endif</td>
-                              <td class="{{$v2_estilo}}">@if($valorf->npe == null) --- @else {{$valorf->npe}} @endif</td>
-                              <td class="{{$v3_estilo}}">@if($valorf->mf == null) --- @else {{$valorf->mf}} @endif</td>
+                  @foreach ($getHistorico as $historico)
 
-                          <?php }
-                                  if($valorf->mf<=4.99 && $valorf->mf!=null){
-                                  //conta disciplinas com negativa
-                                  $count_obs ++;
-                                  //adiciona disciplinas com defices no array
-                                  array_push($defice_disciplinas, $valorf->disciplina->disciplina);
-                                  //faz a verificacao na observacao geral do controlador static, caso encontrar entao esta reprovado a variavel observacao vai ficar true caso nao encontrar prossiga
-                                  $observacao_especifica = ControladorNotas::observacao_especifica($getDirector->turma->classe->id, $getDirector->turma->curso->id, $disciplina["id_disciplina"]);
-                                  }
+                    <tr class="{{$historico->observacao_final}}">
+                      <td>{{$loop->iteration}}</td>
+                      <td>{{$historico->estudante->pessoa->nome}}</td>
+                      <td>{{$historico->estudante->pessoa->genero}}</td>
 
-                                  if($count_obs >= $observacao_geralDB){
-                                      $observacao_final = true;
-                                  }
-                                  if($observacao_especifica){
-                                      $observacao_final = true;
-                                  }
+                      <?php
+                      foreach (Session::get('disciplinas') as $disciplina) {
 
-                                  if($valorf->mf!=null){
-                                  $numero_lancados = $numero_lancados +1;
-                                  }
-                              }
+                          $getCadeiraExame = ControladorStatic::getExameStatus($getDirector->turma->id_curso, $getDirector->turma->id_classe, $disciplina['id_disciplina']);
+                          $getCadeiraRecurso = ControladorStatic::getRecursoStatus($getDirector->turma->id_curso, $getDirector->turma->id_classe, $disciplina['id_disciplina']);
+                          $final = ControladorNotas::getValoresPautaFinalPDF($historico->id_estudante, $disciplina["id_disciplina"], $getDirector->ano_lectivo);
+                          if($final->count() == 0){
+                          ?>
+                          <td>---</td>
+                          @if ($getCadeiraExame)
+                          <td>---</td>
+                          @endif
+                          <td>---</td>
+                          @if ($getCadeiraRecurso)
+                          <td>---</td>
+                          @endif
+                      <?php } else {
+                          foreach ($final as $valorf) {
+                          $v1_estilo = ControladorNotas::nota_10($valorf->mfd);
+                          $v2_estilo = ControladorNotas::nota_10($valorf->npe);
+                          $v3_estilo = ControladorNotas::nota_10($valorf->mf);
+                          $v4_estilo = ControladorNotas::nota_10($valorf->rec);
+                          ?>
+                          <td class="{{$v1_estilo}}">@if($valorf->mfd == null) --- @else {{$valorf->mfd}} @endif</td>
+                          @if ($getCadeiraExame)
+                          <td class="{{$v2_estilo}}">@if($valorf->npe == null) --- @else {{$valorf->npe}} @endif</td>
+                          @endif
+                          <td class="{{$v3_estilo}}">@if($valorf->mf == null) --- @else {{$valorf->mf}} @endif</td>
+                          @if ($getCadeiraRecurso)
+                          <td class="{{$v4_estilo}}">@if($valorf->rec == null) --- @else {{$valorf->rec}} @endif</td>
+                          @endif
 
+                      <?php }
 
-                              }
-                              ?>
-
-                              @if($numero_cadeiras != $numero_lancados)
-                              <td>---</td>
-                              @else
-                              <td class="@if($observacao_final) negativo @else positivo @endif">
-                                  @if($observacao_final)
-                                       NÃO TRANSITA
-                                   @else
-                                   TRANSITA
-                                   @if ($defice_disciplinas)
-                                      [DEF.(
-                                          @foreach ($defice_disciplinas as $item)
-                                             {{strtoupper($item)}},
-                                          @endforeach
-                                          )]
-                                      @endif
-                                   @endif
-
-
-                               </td>
-                              @endif
-
-                        </tr>
-                        @endforeach
-                    </tbody>
-                 </table>
-            </div>
+                          }
+                          }
+                          ?>
+                          <!-- obs -->
+                          <td>-----</td>
+                           <!-- fim obs-->
+                    </tr>
+                    @endforeach
+                </tbody>
+             </table>
          </div>
          <br/><br/>
          <div class="rodape">

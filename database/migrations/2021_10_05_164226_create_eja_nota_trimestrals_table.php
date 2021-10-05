8f2b0526daf4d3519602296765a95e89e9126537
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-05 17:14:37 +0100

    trabalhando no trimestral do eja

--- database/migrations/2021_10_05_164226_create_eja_nota_trimestrals_table.php
+++ database/migrations/2021_10_05_164226_create_eja_nota_trimestrals_table.php
@@ -14,8 +14,30 @@ class CreateEjaNotaTrimestralsTable extends Migration
     public function up()
     {
         Schema::create('eja_nota_trimestrals', function (Blueprint $table) {
-            $table->id();
+            $table->engine="InnoDB";
+            $table->bigIncrements('id');
+            $table->bigInteger('id_estudante')->unsigned()->index();
+            $table->bigInteger('id_disciplina')->unsigned()->index();
+            //nota trimestarl
+            $table->decimal('mes1', 4,2)->nullable();
+            $table->decimal('mes2', 4,2)->nullable();
+            $table->decimal('mes3', 4,2)->nullable();
+
+            $table->decimal('prova', 4,2)->nullable();
+            $table->decimal('subtotal', 4,2)->nullable();
+            $table->decimal('tcp', 4,2)->nullable();
+            $table->decimal('autoav', 4,2)->nullable();
+            $table->decimal('media_trimestre', 4,2)->nullable();
+
+            $table->bigInteger('epoca');
+            $table->string('estado');
+            $table->string('ano_lectivo');
             $table->timestamps();
+        });
+
+        Schema::table('eja_nota_trimestrals', function (Blueprint $table) {
+            $table->foreign('id_estudante')->references('id')->on('estudantes')->onUpdate('cascade');
+            $table->foreign('id_disciplina')->references('id')->on('disciplinas')->onUpdate('cascade');
         });
     }
 

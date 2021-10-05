8645c21992fe7861ee1bc8eeeff7d77004147566
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-05 20:58:46 +0100

    update

--- resources/views/caderneta/list.blade.php
+++ resources/views/caderneta/list.blade.php
@@ -98,9 +98,46 @@ if(($getEpoca1->estado=="on") && ($getEpoca2->estado=="on") && ($getEpoca3->esta
                         $getEstudantes = ControladorStatic::getTotalEstudantesTurma($horario->id_turma, $horario->ano_lectivo);
                         $numero_estudantes = $getEstudantes->count();
                      ?>
+                     <!-- ensino eja -->
                      @if ($horario->turma->curso->id_ensino == 3)
-                        eja
+                     <div class="col-md-4 col-xl-4">
+                        <div class="card widget-card-1">
+                            <div class="card-block-small">
+                                <i class="icofont icofont-file-alt bg-c-yellow card1-icon"></i>
+                            <span class="text-c-pink f-w-600">{{$horario->disciplina->disciplina}}</span>
+                            <h4 style="font-size:20px;">{{$horario->turma->turma}}&nbsp;{{$horario->turma->turno->turno}}</h4>
+                                <div>
+                                    <span class="f-left m-t-10 text-muted">
+                                        Ano: {{$horario->ano_lectivo}} &nbsp;&nbsp; <b>[ {{$numero_estudantes}} ]</b>
+                                        <hr/>
+                                       <div class="operacoes">
+                                       <a href="/cadernetas/ejamensal/{{$horario->id_turma}}/{{$horario->id_disciplina}}/{{$horario->ano_lectivo}}/{{$epoca_usar}}" type="button" class="btn btn-primary btn-icon waves-effect waves-light" data-toggle="tooltip" data-placement="left" title="" data-original-title="Notas do Mês">
+                                            <i class="icofont icofont-edit-alt"></i>
+                                        </a>&nbsp;
+                                        <a href="/cadernetas/ejatrimestral/{{$horario->id_turma}}/{{$horario->id_disciplina}}/{{$horario->ano_lectivo}}/{{$epoca_usar}}" type="button" class="btn btn-primary btn-icon waves-effect waves-light" data-toggle="tooltip" data-placement="left" title="" data-original-title="Notas do Trimestre">
+                                            <i class="icofont icofont-edit-alt"></i>
+                                        </a>&nbsp;
+
+                                        <!--<a href="/minipautas/show/{{$horario->id_turma}}/{{$horario->id_disciplina}}/{{$horario->ano_lectivo}}" type="button" class="btn btn-danger btn-icon waves-effect waves-light" data-toggle="tooltip" data-placement="top" title="" data-original-title="Mini Pauta">
+                                            <i class="icofont icofont-clip-board"></i>
+                                        </a>&nbsp;
+                                        <a href="/estatisticas/minipautas/list/{{$horario->id_turma}}/{{$horario->id_disciplina}}/{{$horario->ano_lectivo}}" type="button" class="btn btn-success btn-icon waves-effect waves-light" data-toggle="tooltip" data-placement="right" title="" data-original-title="Estatística">
+                                            <i class="icofont icofont-chart-bar-graph"></i>
+                                        </a>&nbsp;
+                                        <a href="/cadernetas/printer/{{$horario->id_turma}}/{{$horario->id_disciplina}}/{{$horario->ano_lectivo}}" type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-toggle="tooltip" data-placement="right" title="" data-original-title="Imprimir">
+                                            <i class="ti-printer"></i>
+                                        </a>&nbsp;-->
+                                        <!--<a href="/cadernetas/store/{{$horario->id_turma}}/{{$horario->id_disciplina}}/{{$horario->ano_lectivo}}" type="button" class="btn btn-info btn-icon waves-effect waves-light" data-toggle="tooltip" data-placement="right" title="" data-original-title="Actualizar">
+                                            <i class="icofont icofont-refresh"></i>
+                                        </a>&nbsp;-->
+                                       </div>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                      @else
+                     <!-- outro ensino-->
                      <div class="col-md-4 col-xl-4">
                         <div class="card widget-card-1">
                             <div class="card-block-small">

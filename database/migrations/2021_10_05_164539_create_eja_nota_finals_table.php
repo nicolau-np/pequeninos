3fae17a1129808c7212e4bff4b579e8d308510da
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-05 17:18:19 +0100

    update

--- database/migrations/2021_10_05_164539_create_eja_nota_finals_table.php
+++ database/migrations/2021_10_05_164539_create_eja_nota_finals_table.php
@@ -14,8 +14,20 @@ class CreateEjaNotaFinalsTable extends Migration
     public function up()
     {
         Schema::create('eja_nota_finals', function (Blueprint $table) {
-            $table->id();
+            $table->engine="InnoDB";
+            $table->bigIncrements('id');
+            $table->bigInteger('id_estudante')->unsigned()->index();
+            $table->bigInteger('id_disciplina')->unsigned()->index();
+            $table->decimal('media', 4,2)->nullable();
+            $table->decimal('nf', 4,0)->nullable();
+            $table->string('estado');
+            $table->string('ano_lectivo');
             $table->timestamps();
+        });
+
+        Schema::table('eja_nota_finals', function (Blueprint $table){
+            $table->foreign('id_estudante')->references('id')->on('estudantes')->onUpdate('cascade');
+            $table->foreign('id_disciplina')->references('id')->on('disciplinas')->onUpdate('cascade');
         });
     }
 

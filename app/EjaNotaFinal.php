74836ac8c9ec803ed3f45380a7fd7197f8bd6896
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-05 17:27:24 +0100

    update eja final model

--- app/EjaNotaFinal.php
+++ app/EjaNotaFinal.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class EjaNotaFinal extends Model
 {
-    //
+    protected $table = "eja_nota_finals";
+
+    protected $fillable = [
+        'id_estudante',
+        'id_disciplina',
+        'media',
+        'nf',
+        'estado',
+        'ano_lectivo',
+    ];
+
+    public function estudante(){
+        return $this->belongsTo(Estudante::class, 'id_estudante', 'id');
+    }
+
+    public function disciplina(){
+        return $this->belongsTo(Disciplina::class, 'id_disciplina', 'id');
+    }
 }

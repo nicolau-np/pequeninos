5b63b570db92ecadd479f3dc689681b7b02d652b
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-12 08:22:50 +0100

    update

--- resources/views/principal/consulta.blade.php
+++ resources/views/principal/consulta.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.app_principal')
 @section('content')
 
+<section id="about" class="site-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+
 @endsection

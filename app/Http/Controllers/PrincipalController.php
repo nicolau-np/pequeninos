ee2e885facc00a7f214ef2c793cc4f84b0ba395b
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-12 09:24:51 +0100

    update

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Estudante;
 use Illuminate\Http\Request;
 
 class PrincipalController extends Controller
@@ -32,6 +33,11 @@ class PrincipalController extends Controller
         $request->validate([
             'codigo_acesso'=> ['required', 'string', 'min:6'],
         ]);
+
+        $estudante = Estudante::where(['numero_acesso'=>$request->codigo_acesso])->first();
+        if(!$estudante){
+            return back()->with(['error' => "Código de Acesso Incorrecto"]);
+        }
         $data = [
             'title'=>"SIGE - Sistema de Gestão Escolar",
             'type'=>"consulta",

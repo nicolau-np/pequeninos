8f32393c6e64be15231496d8a9ab90a01adcccdd
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-14 09:59:32 +0100

    update

--- app/Http/Controllers/PhpWordController.php
+++ app/Http/Controllers/PhpWordController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 
 class PhpWordController extends Controller
 {
-    public function declaracaosemnota($id_declaracao){
+    public function declaracaosemnota($id_declaracao)
+    {
 
         $declaracao = Declaracao::find($id_declaracao);
         if (!$declaracao) {
@@ -22,17 +23,47 @@ class PhpWordController extends Controller
 
         $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('word_models/declaracaosemnota.docx');
 
+        /**variaveis */
+        $nome = "[##############]";
+        $pai = "[##############]";
+        $mae = "[##############]";
+        $dia = "[##############]";
+        $mes = "[##############]";
+        $ano = "[##############]";
+        $naturalidade = "[##############]";
+        $provincia = "[##############]";
+        $ano_lectivo = "[##############]";
+        $classe = "[##############]";
+        $turma = "[##############]";
+        $numero = "[##############]";
+        $dia_hoje="[##############]";
+        $mes_hoje="[##############]";
+        $ano_hoje="[##############]";
+        /**fim variaveis */
+
         // Variables on different parts of document
-        $templateProcessor->setValue('nome', $historico->estudante->pessoa->nome);            // On section/content
-        $templateProcessor->setValue('pai', $historico->estudante->pessoa->pai);
-        $templateProcessor->setValue('mae', $historico->estudante->pessoa->mae);           // On footer
+        $templateProcessor->setValue('nome', $nome);            // On section/content
+        $templateProcessor->setValue('pai', $pai);
+        $templateProcessor->setValue('mae', $mae);
+        $templateProcessor->setValue('dia', $dia);            // On section/content
+        $templateProcessor->setValue('mes', $mes);
+        $templateProcessor->setValue('ano', $ano);
+        $templateProcessor->setValue('naturalidade', $naturalidade);            // On section/content
+        $templateProcessor->setValue('provincia', $provincia);
+        $templateProcessor->setValue('ano_lectivo', $ano_lectivo);
+        $templateProcessor->setValue('classe', $classe);            // On section/content
+        $templateProcessor->setValue('turma', $turma);
+        $templateProcessor->setValue('numero', $numero);
+        $templateProcessor->setValue('dia_hoje', $dia_hoje);            // On section/content
+        $templateProcessor->setValue('mes_hoje', $mes_hoje);
+        $templateProcessor->setValue('ano_hoje', $ano_hoje);            // On footer
         // On header
-        $filename = $historico->estudante->pessoa->nome."declaracaosemnota.docx";
+        $filename = $historico->estudante->pessoa->nome . "declaracaosemnota.docx";
         try {
-            $templateProcessor->saveAs('word_models/'.$filename.'.docx');
+            $templateProcessor->saveAs('word_models/' . $filename . '.docx');
         } catch (\Exception $e) {
             echo $e->getMessage();
         }
-        return response()->download('word_models/'.$filename . '.docx')->deleteFileAfterSend(true);
+        return response()->download('word_models/' . $filename . '.docx')->deleteFileAfterSend(true);
     }
 }

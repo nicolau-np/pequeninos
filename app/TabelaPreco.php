b447ea2bc1e89f7370dd287d414796c71fc400c7
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-16 10:59:36 +0100

    adicionando novos campos a tabela de precos para multas

--- app/TabelaPreco.php
+++ app/TabelaPreco.php
@@ -7,29 +7,37 @@ use Illuminate\Database\Eloquent\Model;
 class TabelaPreco extends Model
 {
     protected $table = "tabela_precos";
-    
+
     protected $fillable = [
         'id_tipo_pagamento',
         'id_curso',
         'id_classe',
         'id_turno',
-        'preco', 
+        'preco',
         'forma_pagamento',
+        'multa',
+        'percentagem_multa',
+        'dia_cobranca_multa',
+
     ];
 
-    public function tipo_pagamento(){
+    public function tipo_pagamento()
+    {
         return $this->belongsTo(TipoPagamento::class, 'id_tipo_pagamento', 'id');
     }
 
-    public function curso(){
+    public function curso()
+    {
         return $this->belongsTo(Curso::class, 'id_curso', 'id');
     }
 
-    public function classe(){
+    public function classe()
+    {
         return $this->belongsTo(Classe::class, 'id_classe', 'id');
     }
 
-    public function turno(){
+    public function turno()
+    {
         return $this->belongsTo(Turno::class, 'id_turno', 'id');
     }
 }

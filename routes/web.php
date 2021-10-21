84698abb2604d6f917937111eeca807a33ca4fba
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-21 23:15:00 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -432,11 +432,11 @@ Route::get('test', function () {
                             'estado'=>"on",
                             'ano_lectivo' => $ano_lectivo,
                         ];
-                        if (!Multado::where(['id_estudante' => $estudante->id, 'id_tipo_pagamento' => $tipo_pagamento->id, 'mes_multa' => $epocas->numero, 'ano_lectivo' => $ano_lectivo])->first()) {
+                        /*if (!Multado::where(['id_estudante' => $estudante->id, 'id_tipo_pagamento' => $tipo_pagamento->id, 'mes_multa' => $epocas->numero, 'ano_lectivo' => $ano_lectivo])->first()) {
                             if (Multado::create($data)) {
                                 echo "multado <br/>";
                             }
-                        }
+                        }*/
                     }
                 }
             }

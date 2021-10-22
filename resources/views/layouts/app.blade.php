ee499afc02f671677192ad646704d42c201b1698
Nicolau NP <mr1Normaliii@gmail.com>
2021-10-22 11:31:07 +0100

    eliminando multados no template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,6 @@
 <?php
 use App\Http\Controllers\ControladorStatic;
 $lastYear = ControladorStatic::getLastYear();
-$multados = ControladorStatic::marcarMultas();
 ?>
 <!DOCTYPE html>
 <html lang="{{app()->getLocale()}}">

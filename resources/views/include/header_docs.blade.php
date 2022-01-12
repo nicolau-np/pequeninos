54c8dd6b5e4965d9dfe75d6d054e536d3f0a5eee
Nicolau NP <mr1Normaliii@gmail.com>
2022-01-12 20:29:23 +0000

    update insignea

--- resources/views/include/header_docs.blade.php
+++ resources/views/include/header_docs.blade.php
@@ -1,12 +1,11 @@
 <div>
 
     <p style="text-align: center;">
-        <img src="https://www.angop.ao/site/assets/files/3759/insignia.437x0-is-pid1464.png"
-        alt="" srcset="" style="height:40px; width:40px;"/><br/>
-        REPÚBLICA DE ANGOLA<br/>
-        MINISTÉRIO DA EDUCAÇÃO CIÊNCIA E TECNOLOGIA<br/>
-        COMPLEXO ESCOLAR SOS DO HUAMBO<br/>
+        <img src="https://sigeokussoleka.com/imagesURL/insignea.png" alt="" srcset=""
+            style="height:40px; width:40px;" /><br />
+        REPÚBLICA DE ANGOLA<br />
+        MINISTÉRIO DA EDUCAÇÃO CIÊNCIA E TECNOLOGIA<br />
+        COMPLEXO ESCOLAR SOS DO HUAMBO<br />
         = HUAMBO =
     </p>
 </div>
-
